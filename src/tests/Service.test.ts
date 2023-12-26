bee4d3aa7b2fb8a9224269e35cff8b58bd824cb3
Christophe CAMENSULI <ccamensuli@gmail.com>
2023-12-26 20:54:55 +0100

    test: add service test

--- src/tests/Service.test.ts
+++ src/tests/Service.test.ts
@@ -1,2 +1,33 @@
 import 'mocha';
-//import assert from 'node:assert'
+import assert from 'node:assert'
+import Service  from '../Service'
+
+
+declare global {
+  interface NodeJSGlobal {
+    service: Service;
+  }
+}
+declare let global: NodeJS.Global & { service?: Service };
+
+
+describe("NODEFONY Service", () => {
+
+  describe("namespace", () => {
+
+     before(() => {
+      global.service = new Service("test");
+    });
+    it("register", (done) => {
+      assert(Service);
+      assert(global.service instanceof Service);
+      done();
+    });
+
+  })
+
+
+
+
+
+})
