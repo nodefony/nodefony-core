cd81bc5c68707ef9ae1b56405d74ed4d2fe80f67
Christophe CAMENSULI <ccamensuli@gmail.com>
2023-12-28 17:41:31 +0100

    fix: types in package

---
+++ src/types/globals.d.ts
@@ -0,0 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+export interface nodefonyOptions {
+    [key: string]: any;
+}
+
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,8 +1,43 @@
-export * from '../../dist/index.d.ts';
-export * from './globals.d.ts';
-export * from './Nodefony.d.ts';
-export * from './Tools.d.ts';
-export * from './Syslog.d.ts';
-export * from './Container.d.ts';
-export * from './Service.d.ts';
-export * from './Event.d.ts';
+// Importez chaque déclaration de type individuelle
+import  globals  from './globals';
+
+import  index  from '../../dist/types/src/index';
+import  container  from '../../dist/types/src/Container';
+import  error  from '../../dist/types/src/Error';
+import  event  from '../../dist/types/src/Event';
+import  fileclass  from '../../dist/types/src/FileClass';
+import  kernel  from '../../dist/types/src/Kernel';
+import  nodefony  from '../../dist/types/src/Nodefony';
+import  service  from '../../dist/types/src/Service';
+import  tool  from '../../dist/types/src/Tools';
+import  syslog  from '../../dist/types/src/syslog/Syslog';
+import  pdu  from '../../dist/types/src/syslog/Pdu';
+import  file    from '../../dist/types/src/finder/File';
+import  fileResult    from '../../dist/types/src/finder/FileResult';
+import  Finder    from '../../dist/types/src/finder/Finder';
+import  result    from '../../dist/types/src/finder/Result';
+import  Command    from '../../dist/types/src/command/command';
+import  task    from '../../dist/types/src/command/task';
+
+
+// Exportez tous les types
+export { 
+  globals, 
+  index, 
+  container, 
+  error, 
+  event, 
+  fileclass, 
+  kernel, 
+  nodefony, 
+  service, 
+  tool, 
+  syslog, 
+  pdu,
+  file,
+  fileResult,
+  Finder,
+  result,
+  Command,
+  task
+};
